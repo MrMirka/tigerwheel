a4b7130aad1d5e3f5681d2333b25b92236a476d3
MrMirka <aleshin.dev@gmail.com>
2024-05-06 17:20:10 -0400

    add gsap

--- scripts.js
+++ scripts.js
@@ -8,13 +8,15 @@ const sprites = {}; //Хранилище текстур
 
 
 const position = [
-    {name: 'Bonus 100', position: 0},
-    {name: '50FS', position: 60},
-    {name: 'Bonus 75', position: 120},
-    {name: '100FS_1', position: 180},
-    {name: 'Bonus 50', position: 240},
-    {name: '100FS_2', position: 300}
+    { name: 'Bonus 100', position: 0 },
+    { name: '50FS', position: 60 },
+    { name: 'Bonus 75', position: 120 },
+    { name: '100FS_1', position: 180 },
+    { name: 'Bonus 50', position: 240 },
+    { name: '100FS_2', position: 300 }
 ];
+
+
 
 
 //Базовые параметры скрипта
@@ -22,13 +24,13 @@ let param = {
     startPosition: 0, //From
     targetPosition: degreesToRadians(position[5].position), //To
     duration: 350,
-    blurAngle: 2.3,
+    blurAngle: 1.3,
     imgBaraban: './img/tiger/baraban.png',
     imgBack: './img/tiger/barabanBackground.png',
     imgLogo: './img/tiger/logo.png',
     imgArrow: './img/tiger/pointer.png',
-    width: document.getElementById('c').offsetWidth *  window.devicePixelRatio,
-    height: document.getElementById('c').offsetHeight *  window.devicePixelRatio
+    width: document.getElementById('c').offsetWidth * window.devicePixelRatio,
+    height: document.getElementById('c').offsetHeight * window.devicePixelRatio
 };
 //Установливает рандомную цель (тестовая)
 setTarget();
@@ -54,15 +56,15 @@ initLoaderCircle();
 
 
 //Запуск лупа
-initLoop();
+//initLoop();
 
 
 //Loaders
 const loader = new PIXI.Loader();
 loader.add('wheelBack', param.imgBack)
-      .add('wheel', param.imgBaraban)
-      .add('wheelLogo', param.imgLogo)
-      .add('wheelArrow', param.imgArrow);
+    .add('wheel', param.imgBaraban)
+    .add('wheelLogo', param.imgLogo)
+    .add('wheelArrow', param.imgArrow);
 
 loader.load((loader, resources) => {
     //Код выполняется после загрузки изображений
@@ -101,37 +103,86 @@ loader.load((loader, resources) => {
     sprites.wheel.filters = [radialBlur];
     radialBlur.angle = 0;
     radialBlur.kernelSize = 120;
-    radialBlur.center = [param.width / 2,param.height / 2];
+    radialBlur.center = [param.width / 2, param.height / 2];
     radialBlur.radius = param.width / 2;
-    container.transform.position.set(param.width / 2,param.height / 2);
+    container.transform.position.set(param.width / 2, param.height / 2);
 
     //Скрываем графический лоадер 
     removeLoader();
 
     //Mouse listener
-    window.addEventListener('mousedown', () => { toRun = true; });
-    window.addEventListener("touchstart", () => { toRun = true; });
-});      
+    window.addEventListener('mousedown', () => { 
+        toRun = true; 
+        sprites.wheel.rotation = param.startPosition;
+        let blurCount = 0;
+          gsap.to(sprites.wheel, {
+            rotation: param.targetPosition + Math.PI * 4, // Задаем конечное значение угла в радианах
+            duration:3, // Длительность в секундах
+            ease: "power1.inOut", // Тип easing
+            onComplete: function () {
+                param.startPosition = param.targetPosition
+                setTarget()
+            }
+        });  
+        /*  gsap.to(sprites.wheel, {
+            rotation: param.targetPosition + Math.PI * 4, // Добавляем 4π радианов для двух дополнительных оборотов
+            duration: 5, // Длительность анимации в секундах
+            ease: "power1.inOut", // Тип easing для плавного вращения
+            onUpdate: function() {
+                // Увеличиваем значение счётчика
+                blurCount += 0.1; // Настраиваем шаг для плавного изменения
+        
+                // Применяем размытие на основе синусоидальной функции
+                radialBlur.angle = Math.abs(Math.sin(blurCount)) * param.blurAngle;
+            },
+            onComplete: function () {
+               
+                param.startPosition = param.targetPosition
+                setTarget(); // Опционально устанавливаем следующую цель вращения
+            }
+        });  */
+    });
+    window.addEventListener("touchstart", () => { 
+        toRun = true;
+        sprites.wheel.rotation = param.startPosition;
+        let blurCount = 0;
+          gsap.to(sprites.wheel, {
+            rotation: param.targetPosition + Math.PI * 4, // Задаем конечное значение угла в радианах
+            duration:3, // Длительность в секундах
+            ease: "power1.inOut", // Тип easing
+            onComplete: function () {
+                param.startPosition = param.targetPosition
+                setTarget()
+            }
+        });  
+     });
+});
+
+
 
 
 
 
 //Инициализируем анимацию, движение колеса когда toRun = true
 function initLoop() {
+
     let countShift = (1 + param.blurAngle) / param.duration;
     app.ticker.add(() => {
-        //Loader
-        if(loaderBlock != undefined){
-            loaderBlock.rotation += 0.15;
-         }
 
-        if(toRun){
-            if(!isMobileDevice())
-            radialBlur.angle = CubicInOut(Math.abs(Math.sin(count)) * param.duration, 0, param.blurAngle, param.duration);
+        //Loader
+        if (loaderBlock != undefined) {
+            loaderBlock.rotation += 0.15;
+        }
+
+        if (toRun) {
+
+            
+            if (!isMobileDevice())
+              radialBlur.angle = CubicInOut(Math.abs(Math.sin(count)) * param.duration, 0, param.blurAngle, param.duration);
             sprites.wheel.rotation = CubicInOut(n, param.startPosition, param.targetPosition - param.startPosition + (Math.PI * 8), param.duration);
             n++;
             count += countShift;
-            if(n === param.duration) {
+            if (n === param.duration) {
                 count = 0;
                 toRun = false;
                 n = 1;
@@ -143,46 +194,46 @@ function initLoop() {
 }
 
 //Изинг
-function CubicInOut(t, b, c, d){
-	if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
-	return c / 2 * ((t -= 2) * t * t + 2) + b;
+function CubicInOut(t, b, c, d) {
+    if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+    return c / 2 * ((t -= 2) * t * t + 2) + b;
 }
 
 //Тестовая функция для рандомного положения барабана (заменить на нужное поведение) 
-function setTarget(){
+function setTarget() {
     let pos = Math.floor(Math.random() * 5);
     let shift = (Math.random() - 0.5) * 0.17;
-    if(shift > 0.12) {
+    if (shift > 0.12) {
         shift = 0.12;
-    }else if(shift < -0.12){
+    } else if (shift < -0.12) {
         shift = -0.12;
     }
     param.targetPosition = degreesToRadians(position[pos].position) + shift;
 
     let str = 'Кручу на  ' + position[pos].name;
     console.log(str);
-    
+
 }
 
-function updateParam(target){
+function updateParam(target) {
     sprites.wheel.rotation = target; //Обнуляем количесво оборотов колеса (чтобы счисление было в пределах 0 - 2PI)
     param.startPosition = target; //Устанавливаем текущее положение в стартовое
 }
 
 //Проверка на мобильные устройства
 //Блур включаем только на десктопах и айфонах
-function isMobileDevice(){
+function isMobileDevice() {
 
-	if( /Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		return true;
-	}else{
-		return false;
-	}
+    if (/Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 //Квадратный лоадер
-function initLoader(){
+function initLoader() {
     loaderBlock = new PIXI.Container();
     loaderBlock.position.x = param.width / 2;
     loaderBlock.position.y = param.height / 2;
@@ -207,7 +258,7 @@ function initLoader(){
 
 
 //Круглый лоадер
-function initLoaderCircle(){
+function initLoaderCircle() {
     loaderBlock = new PIXI.Container();
     loaderBlock.position.x = param.width / 2;
     loaderBlock.position.y = (param.height / 2) * 0.97;
@@ -218,7 +269,7 @@ function initLoaderCircle(){
     let size = param.width * 0.08;
     let loaderS = new PIXI.Graphics();
     loaderS.lineStyle(size * 0.2, 0xFF0025);
-    loaderS.arc(0, 0, size, 0, Math.PI*2);
+    loaderS.arc(0, 0, size, 0, Math.PI * 2);
     loaderS.lineStyle(size * 0.2, 0xF7E800);
     loaderS.arc(0, 0, size, 0, Math.PI * 0.4);
     loaderBlock.addChild(loaderS);
@@ -226,10 +277,12 @@ function initLoaderCircle(){
 
 
 //Удаляем лоадер
-function removeLoader(){
+function removeLoader() {
     app.stage.removeChild(loaderBlock);
 }
 
 function degreesToRadians(degrees) {
     return degrees * (Math.PI / 180);
 }
+
+
